855d1c47b48a855c0d605956a4c31db598dfe1b5
Daniel Driskill <danieldriskill@Daniels-MacBook-Pro-2.local>
2021-03-12 20:11:17 -0700

    Add 'inc' query param for the properties we need.

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -7,8 +7,10 @@ router.get("/employees", async (req, res) => {
     let results = req.query.results || "20";
     const page = req.query.page || "1";
     try {
-        // Grabbing users from randomuser.me
-        const { data } = await axios.get(`https://randomuser.me/api/?page=${page}&results=${results}&seed=abc`);
+        // Grabbing users from randomuser.me with needed data properties
+        const { data } = await axios.get(
+            `https://randomuser.me/api/?page=${page}&results=${results}&seed=abc&inc=picture,name,email,location,phone,dob`
+        );
         res.send(data);
     } catch (error) {
         throw new Error(error);
