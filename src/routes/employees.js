dc15b540f22c4787eb765f5e44702d176fb5e606
Daniel Driskill <danieldriskill@Daniels-MacBook-Pro-2.local>
2021-03-12 19:47:56 -0700

    Add pagination functionality.

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -3,12 +3,13 @@ const router = new express.Router();
 const axios = require("axios");
 
 // Get employees
-// TODO: Allow query params to filter/paginate
 router.get("/employees", async (req, res) => {
-    const results = req.query.results || "20";
+    let results = req.query.results || "20";
+    const page = req.query.page || "1";
+    // TODO: Search/filter functionality
     try {
         // Grabbing users from randomuser.me
-        const { data } = await axios.get(`https://randomuser.me/api/?results=${results}`);
+        const { data } = await axios.get(`https://randomuser.me/api/?page=${page}&results=${results}&seed=abc`);
         res.send(data);
     } catch (error) {
         throw new Error(error);
